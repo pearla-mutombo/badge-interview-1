6ebae600c53c0792e4e2631a077369bc2d5ea0ad
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-26 20:56:41 -0400

    completed lesson 02 -level 03

---
+++ lesson-02-npm/level-03-named-imports/messages.js
@@ -0,0 +1,3 @@
+export const message1 = " to export a named export it has to be exported in the name variable or function that is being exported. and it has ti match the name and be in the same line.";
+
+export const message2 = "to use names import you must use curly braces that is ecported for exmaple 'import { myValue} from a .js file";
