fa6e4f105d7f55bc123c573f90f18f78797e97bd
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-27 18:11:42 -0400

    complered lesson 1 -level 02 and level 03

---
+++ lesson-01-nodejs/level-02-nodejs-script/script.js
@@ -0,0 +1,3 @@
+console.log(
+  "To run JavaScript in the terminal, first you have navigate to the specific folder and find the file name ending i .js such as script.js. select the dropdown, choose the terminal that you want to run your script. check your code then run script by writing node file.js (or the name of the js file) and run.",
+);
--- lesson-01-nodejs/level-03-no-dom/script.js
+++ lesson-01-nodejs/level-03-no-dom/script.js
@@ -1,1 +1,3 @@
 console.log("Attempting to access browser features...");
+localStorage.setItem("message", "Welcome to Badge Interview 1!");
+const pTag = document.querySelector("p");
