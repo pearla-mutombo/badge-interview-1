29d464eef7cad0fd90c2465aa6fa880e75dfa253
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-27 18:12:14 -0400

    completed lesson 02 - level 1 and level 05

--- lesson-02-npm/level-01-exports-imports/index.js
+++ lesson-02-npm/level-01-exports-imports/index.js
@@ -6,3 +6,8 @@ import names from "./names.js";
 greet("Frontend Developer");
 stats(3, 7);
 showNames(names);
+
+console.log(
+  "exporting allow your code be in a single file and makes it easier to access the other files.",
+  "importing allows you to access code form another file. and both are a way to seperate codes into different files or modules so that it can be reused.",
+);
---
+++ lesson-02-npm/level-05-npm-command/script.js
@@ -0,0 +1,3 @@
+console.log(
+  "NPM command helps you to start a new project once it it installed in the terminal.  it also autogenerate a file named package.json and this .json package you see different response in the file that you answered in your termial while installing it by writing npm init. and in this package you can change or revise sepecific elements such as 'type', 'author' and  add you repositiory url.",
+);
