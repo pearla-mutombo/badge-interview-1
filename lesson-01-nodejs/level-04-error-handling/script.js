6173e70d4bd20f90a6c2f5dbf5cb822483e68b1b
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-26 20:56:20 -0400

    completed lesson 1 -level 02, level 04, and level 05

--- lesson-01-nodejs/level-04-error-handling/script.js
+++ lesson-01-nodejs/level-04-error-handling/script.js
@@ -1,1 +1,7 @@
-console.log("Attempting to access browser features...");
+try {
+  console.log("Attempting to access browser features...");
+  localStorage.setItem("message", "Welcome to level 3!");
+  const pTag = document.querySelector("p");
+} catch (error) {
+  console.log("localStorage not define.");
+}
--- lesson-01-nodejs/level-05-debug-terminal/script.js
+++ lesson-01-nodejs/level-05-debug-terminal/script.js
@@ -1,2 +1,5 @@
 console.log("Roses are red.");
 console.log("Violets are blue.");
+console.log(
+    "To use the javaScript debugger you hav first - click or select a breakpoint to start with. then you hvae to establish a JavaScript Debug terminal, then you can run the script by writing node script.js in the terminal. but before that make sure that you are in the correct folder to navigate. and you can choose to step into to run each line of your code. or simply click the green play button."
+);
